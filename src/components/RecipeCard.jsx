5e984db05f7e2fac2d3954d1f117c9cec335ed7d
Andres Paz <andres.paz997@gmail.com>
2025-05-21 23:46:28 -0500

    style: para mostrar peso y precio ademas de incluir sección de información nutricional

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -20,7 +20,47 @@ export default function RecipeCard({ recipe, addToCart }) {
                     {recipe.name}
                 </Link>
             </h3>
-            
+            <div className="weight">({recipe.weight})</div>
+            <div className="price">${recipe.price}</div>
+
+            <section className="nutrition-info" aria-label="Información nutricional">
+                <div className="nutrition-item">
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/883/883407.png"
+                        alt="Calorías"
+                        style={{ width: 28, height: 28 }}
+                    />
+                    <span className="value">{recipe.calories}</span>
+                    <span className="label">Calorías</span>
+                </div>
+                <div className="nutrition-item">
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/1046/1046784.png"
+                        alt="Grasas"
+                        style={{ width: 28, height: 28 }}
+                    />
+                    <span className="value">{recipe.fats}g</span>
+                    <span className="label">Grasas</span>
+                </div>
+                <div className="nutrition-item">
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/1046/1046787.png"
+                        alt="Carbohidratos"
+                        style={{ width: 28, height: 28 }}
+                    />
+                    <span className="value">{recipe.carbs}g</span>
+                    <span className="label">Carbohidratos</span>
+                </div>
+                <div className="nutrition-item">
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/1046/1046793.png"
+                        alt="Proteínas"
+                        style={{ width: 28, height: 28 }}
+                    />
+                    <span className="value">{recipe.proteins}g</span>
+                    <span className="label">Proteínas</span>
+                </div>
+            </section>
 
 
             <Link to={`/recipe/${encodeURIComponent(recipe.name)}`}>
