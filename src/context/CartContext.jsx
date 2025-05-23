9a49263c5b64d1760156e35148acc3db6bfcb649
Andres Paz <andres.paz997@gmail.com>
2025-05-22 22:28:13 -0500

    feat: hace que addToCart agregue o incremente productos en el carrito

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,19 +3,28 @@ import React, { createContext, useState } from 'react';
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
- 
-  const totalQuantity = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+    const [cartItems, setCartItems] = useState([]);
 
-return (
-  <CartContext.Provider value={{ cartItems, setCartItems, totalQuantity }}>
-    {children}
-  </CartContext.Provider>
-);
+    const totalQuantity = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+    
+    const addToCart = (product) => {
+        setCartItems(prevItems => {
+            const index = prevItems.findIndex(item => item.name === product.name);
+            if (index >= 0) {
+                const newItems = [...prevItems];
+                newItems[index].quantity += 1;
+                return newItems;
+            }
+            return [...prevItems, { ...product, quantity: 1 }];
+        });
+    };
+    return (
+        <CartContext.Provider value={{ cartItems, setCartItems, addToCart, totalQuantity }}>
+            {children}
+        </CartContext.Provider>
+    );
+};
 
 
-return (
-    <CartContext.Provider value={{ cartItems, setCartItems, totalQuantity }}>
-      {children}
-    </CartContext.Provider>
-  );
+
+
