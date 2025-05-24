049e451f9ec7317c67863653ef9d3993b137739c
LuisUvE <luiseug2002@gmail.com>
2025-05-24 15:07:55 -0500

    Configuracion de las rutas principales y los providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import { RequirementsProvider } from './context/RequirementsContext';
+import Cart from './components/Cart';
+import Checkout from './pages/Checkout';
+import RecipeDetail from './pages/RecipeDetail';
+import Requirements from './pages/Requirements';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RequirementsProvider>
+      <CartProvider>
+        <Cart />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Requirements />} />
+            <Route path="/products" element={<Checkout />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </RequirementsProvider>
   );
 }
 
